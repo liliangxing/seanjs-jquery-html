d76c141ca598116ff1d89d16ae172355b35bcbf3
liliangxing <253254457@qq.com>
2020-01-02 11:22:20 +0800

    返回问题，解决。根据scrollFlag

--- public/static/home/defaults/beizhi/content.js
+++ public/static/home/defaults/beizhi/content.js
@@ -73,12 +73,34 @@ $(function(){
         var key = e.which;
         if (key == 13) highlight();
     });
+
     $(".ui-btn-left_pre").click(function(){
         var   backUrl= $(this).attr('data-url');
-        if(history.length > 0){
-            window.history.go( -1 );
+        if(scrollFlag){
         }else{
             window.location.href =backUrl;
+            return;
+        }
+        if ((navigator.userAgent.indexOf('MSIE') >= 0) && (navigator.userAgent.indexOf('Opera') < 0)){ // IE
+            if(history.length > 0){
+                window.history.go( -1 );
+            }else{
+                window.location.href =backUrl;
+            }
+        }else{ //非IE浏览器
+            if (navigator.userAgent.indexOf('Firefox') >= 0 ||
+                navigator.userAgent.indexOf('Opera') >= 0 ||
+                navigator.userAgent.indexOf('Safari') >= 0 ||
+                navigator.userAgent.indexOf('Chrome') >= 0 ||
+                navigator.userAgent.indexOf('WebKit') >= 0){
+                if(window.history.length > 1){
+                    window.history.go( -1 );
+                }else{
+                    window.location.href =backUrl;
+                }
+            }else{ //未知的浏览器
+                window.history.go( -1 );
+            }
         }
     });
 });
