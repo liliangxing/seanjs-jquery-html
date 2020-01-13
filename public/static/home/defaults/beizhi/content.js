82f54cc40df3d924bc6ff697386a6007c6ab7039
liliangxing <253254457@qq.com>
2020-01-13 18:11:56 +0800

    简单

--- public/static/home/defaults/beizhi/content.js
+++ public/static/home/defaults/beizhi/content.js
@@ -254,29 +254,6 @@ function overrrideUrl(pageIndex) {
     var   newUrl=  changeURLArg(url, "p", pageIndex);
     window.history.pushState({}, "", newUrl);
 }
-function changeURLArg(url, arg, arg_val) {
-    /// <summary>
-    /// url参数替换值
-    /// </summary>
-    /// <param name="url">目标url </param>
-    /// <param name="arg">需要替换的参数名称</param>
-    ///<param name="arg_val">替换后的参数的值</param>
-    /// <returns>参数替换后的url </returns>
-    var pattern = arg + '=([^&]*)';
-    var replaceText = arg + '=' + arg_val;
-    if (url.match(pattern)) {
-        var tmp = '/(' + arg + '=)([^&]*)/gi';
-        tmp = url.replace(eval(tmp), replaceText);
-        return tmp;
-    } else {
-        if (url.match('[\?]')) {
-            return url + '&' + replaceText;
-        } else {
-            return url + '?' + replaceText;
-        }
-    }
-    return url + '\n' + arg + '\n' + arg_val;
-}
 //获取url中的参数
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
