5081eebd3e0dccd2b428c4cad61fbc08613f1428
liliangxing <253254457@qq.com>
2020-01-14 17:37:07 +0800

    简单

--- public/static/home/defaults/beizhi/content.js
+++ public/static/home/defaults/beizhi/content.js
@@ -44,7 +44,7 @@ $(function(){
             break;
     }
     function dosmlyyl(){
-        $("#content").prepend("<p>来源：<a href='http://www.amtb.org.tw/bt/amtb_jindian.asp?web_choice=26&web_amtb_index=266' target='_blank'>http://www.amtb.org.tw/bt/amtb_jindian.asp?web_choice=26&web_amtb_index=266</a></p>");
+        $("#content").prepend("<p>来源：<br><a href='http://www.amtb.org.tw/bt/amtb_jindian.asp?web_choice=26&web_amtb_index=266' target='_blank'>http://www.amtb.org.tw/bt/amtb_jindian.asp?web_choice=26&web_amtb_index=266</a></p>");
     }
     function dobet(){
         $("#content").prepend("<p>来源：<br><a href='http://www.amtb.org.tw/rsd/jiangtang_ch.asp?web_choice=99&web_rel_index=3970' target='_blank'>http://www.amtb.org.tw/rsd/jiangtang_ch.asp?web_choice=99&web_rel_index=3970</a></p>");
