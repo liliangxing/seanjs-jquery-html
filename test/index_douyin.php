009a42c1531d567b8bf01968a2ef6d1031cd9138
liliangxing <253254457@qq.com>
2020-05-20 16:53:47 +0800

    1

--- test/index_douyin.php
+++ test/index_douyin.php
@@ -2,8 +2,13 @@
 session_start();
 $video="http://cdn-media.jingzong.org/mp4/02/02-041/02-041-0508.mp4";
 $title="视频";
+$cover_path="/public/static/home/defaults/projekktor/intro.png";
 if (isset($_GET['video'])==TRUE) {$video=urldecode($_GET['video']); }
-if (isset($_GET['title'])==TRUE) {$title="[".$title."]".urldecode($_GET['title']); }
+$get_title =  null;
+if (isset($_GET['title'])==TRUE) {
+    $get_title =  urldecode($_GET['title']);
+    $title="[".$title."]".$get_title; }
+if (isset($_GET['cover_path'])==TRUE) {$cover_path=urldecode($_GET['cover_path']);}
 ?>
 <html>
 <head>
@@ -19,12 +24,15 @@ if (isset($_GET['title'])==TRUE) {$title="[".$title."]".urldecode($_GET['title']
     <script type="text/javascript" src="/public/static/home/defaults/beizhi/content_common.js"></script>
     <title><?php echo $title;?></title>
 </head>
-<body>
 <body id="news">
+<div class="cover" <?php if (isset($_GET['from'])==TRUE){echo "style=\"display: none\"";}?>>
+    <div><img width="100%" src="<?php echo $cover_path;?>" /></div>
+    <div class="text_down"><?php echo $get_title;?></div>
+</div>
 <div class="Listpage">
 <link href="/public/static/home/defaults/projekktor/projekktor.style.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="/public/static/home/defaults/projekktor/projekktor-1.3.09.js"></script>
-<video id="player_a" class="projekktor" poster="/public/static/home/defaults/projekktor/intro.png"
+<video id="player_a" class="projekktor" poster="<?php echo $cover_path;?>"
        title="<?php echo $video;?> " style="width:100%;height:44em">
 </video>
 <script type="text/javascript">
@@ -41,7 +49,8 @@ if (isset($_GET['title'])==TRUE) {$title="[".$title."]".urldecode($_GET['title']
 </script>
 
  <div class="page-bizinfo">
-    <div class="text_down" style="word-wrap: break-word">下载地址：<br/>
+     <div class="text_down" ><?php echo $get_title;?></div>
+     <div class="text_down" style="word-wrap: break-word">下载地址：<br/>
         <a href="<?php echo $video;?> "><?php echo $video;?> </a> <br/>(请用在新的浏览器打开下载)
     </div>
  </div>
