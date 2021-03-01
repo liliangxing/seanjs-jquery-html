2fd682929f3b6370c83e2ab187e2dc6ab4c533f3
liliangxing <253254457@qq.com>
2021-03-01 09:20:42 +0800

    height:48em

--- test/index_douyin.php
+++ test/index_douyin.php
@@ -38,7 +38,7 @@ if (isset($_GET['cover_path'])==TRUE) {$cover_path=urldecode($_GET['cover_path']
 <link href="/public/static/home/defaults/projekktor/projekktor.style.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="/public/static/home/defaults/projekktor/projekktor-1.3.09.min.js"></script>
     <video id="player_a" class="projekktor" poster="<?php echo $cover_path;?>"
-       title="<?php echo $video;?> " style="width:100%;height:44em" controls>
+       title="<?php echo $video;?> " style="width:100%;height:48em" controls>
 </video>
 <script type="text/javascript">
     $(document).ready(function () {
